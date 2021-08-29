b7df45aa8a6ab7b3d238346facd98fd8d08939d4
Hidayt Rahman <hidaytrahman@gmail.com>
2021-08-29 11:18:04 +0530

    Updates

--- src/components/shared/Loader.js
+++
@@ -1,5 +0,0 @@
-import { CircularProgress } from "@material-ui/core";
-
-const Loader = ({loading}) => loading && <CircularProgress color="secondary" />
-
-export default Loader;
--- src/core/constant.js
+++
@@ -1,3 +0,0 @@
-export const API = {
-    baseUrl: "https://aladdinbtest.azurewebsites.net/api/"
-}
