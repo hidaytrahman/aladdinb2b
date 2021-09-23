dc1895422778408a6a56db35d404e4f78d2cd199
Hidayt Rahman <hidaytrahman@gmail.com>
2021-09-23 23:54:42 +0530

    added all material dependencies as a different module

---
+++ src/core/mui.js
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@material-ui/core";
+
+export function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography component="span">{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+
+ export function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
